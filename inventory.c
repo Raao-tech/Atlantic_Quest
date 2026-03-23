839ac1e5589e228aafb5b4fb9830019ab2ae9d63
VioletaLZ <violeta.lozano@estudiante.uam.es>
2026-03-23 12:02:11 +0100

    Inventory_modificado

--- inventory.c
+++ inventory.c
@@ -42,49 +42,51 @@ Status inventory_destroy(Inventory *inventory){
 }
 
 
-Status	inventory_add(Set *pset, Id new_id){
-	Id* pset_temp = NULL;
-	/* If pset or new_id don't exist  */
-	if(pset == NULL || new_id == NO_ID) return	ERROR;
-
-	if( set_contains_id(pset, new_id) == TRUE) return OK;
-
-	//agregamos un espacio en el conjutno de objetos
-	pset_temp = (Id*)realloc((void *)pset->ids, (sizeof(Id) * (pset->n_ids + 1)) );
+Status inventory_add(Inventory *inventory, Id new_id){
 	
-	if(pset_temp == NULL) return ERROR;
-	
-	pset_temp[pset->n_ids] = new_id;
-	pset->n_ids++; 
+	if(inventory == NULL || new_id == NO_ID) return	ERROR;
 
-	pset->ids = pset_temp;
+  if (set_get_n_ids(inventory->objs) >= inventory->max_objs) return FULL_OF_LOVE;
 
-	return	OK;
+	return	set_add(inventory->objs, new_id);
 }
 
-Status	set_delete_id(Set *pset, Id trash_id){
-	int	i;
-	if(!pset || trash_id == NO_ID) return ERROR;
+Status	inventory_delete_obj(Inventory *inventory, Id trash_id){
 
-	for (i = 0; i < pset->n_ids; i++){
-		if(pset->ids[i] == trash_id) return set_reduce(pset,i);
-	}
+  if(!inventory || trash_id == NO_ID) return ERROR;
 
-	return ERROR;
+	return set_delete_id(inventory->objs, trash_id);
 }
-
-
 
 /* id */
 Status inventory_set_max_objs(Inventory *inventory, int max){
-  if (!inventory) return ERROR;
+  if (!inventory || max < 0) return ERROR;
   inventory->max_objs = max;
   return OK;
 }
-Id inventory_get_id(Inventory *inventory){
-  if (obj == NULL) return NO_ID;
-  return obj->id;
+
+int inventory_get_max_objs(Inventory* inventory){
+
+  if (!inventory) return ERROR;
+  
+  return inventory->max_objs;
+
 }
+
+int inventory_get_n_ids(Inventory *inventory){
+  if (!inventory) return -1;
+
+  return set_get_n_ids(inventory->objs);
+
+}
+
+Id	inventory_get_id_at(Inventory *inventory, int position){
+	if(!inventory || position < 0 || position >= inventory_get_n_ids(inventory)) return NO_ID;
+
+	return set_get_id_at(inventory, position);
+}
+
+
 
 
 /* name */
@@ -95,6 +97,7 @@ Status obj_set_name(Object *obj, char *name){
 
   return OK;
 }
+
 Bool obj_has_name(Object *obj, char *name){
     if(!obj || !name) return FALSE;
     return (strcmp(obj->name, name) == 0) ? TRUE : FALSE;

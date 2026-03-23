56dbcc56bb4c8c619f1e4d1774764e939d35ec76
Raao <rafaelaraujordono@gmail.com>
2026-03-23 11:59:53 +0100

    Add files via upload

---
+++ links.c
@@ -0,0 +1,16 @@
+/**
+ * @brief It implements the entity struct
+ *
+ * @file character.c
+ * @author Violeta
+ * @version 0
+ * @date 04-02-2025
+ * @copyright GNU Public License
+ */
+
+ #include "links.h"
+
+ struct link{
+    
+};
+ 

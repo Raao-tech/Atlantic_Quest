f049e1df3a6a29b16696214decb27f710d289281
Ao6400 <salvador.alcala.alvarez@outlook.com>
2026-03-25 16:12:56 +0100

    Add files via upload
    Añadida función contains y otras implementadas en el .c, aun falta comentarlo
    25/3/2026
    Salvador

--- headers/inventory.h
+++ headers/inventory.h
@@ -1,3 +1,13 @@
+/**
+ * @brief It implements the player struct
+ *
+ * @file player.c
+ * @author Violeta, Rafael y Salvador
+ * @version 1
+ * @date 25-03-2026
+ * @copyright GNU Public License
+ */
+
 #ifndef INVENTORY_H
 #define INVENTORY_H
 
@@ -5,19 +15,23 @@
 
 typedef struct _Inventory Inventory;
 
-Inventory* inventroy_create();
+Inventory* inventory_create();
 
 Status inventory_destroy(Inventory* inventory);
 
 Status inventory_add(Inventory* inventory, Id new_id);
 
-Status inventory_contains_object(Inventory* inventory, Id* obj);
+Status inventory_delete_obj(Inventory *inventory, Id trash_id);
 
-Status inventory_delete_object(Inventory* inventory, Id* obj);
+Bool inventory_contains_object(Inventory* inventory, Id* obj);
 
 Status inventory_set_max_objs(Inventory *inventory, int max);
 
 int inventory_get_max_objs(Inventory* inventory);
+
+int inventory_get_n_ids(Inventory *inventory);
+
+Id	inventory_get_id_at(Inventory *inventory, int position);
 
 Status inventory_print(Inventory *inventory);
 

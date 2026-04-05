f099178b501d1b5d84272d248c7f7f5ba966f7c9
Raao <rafaelaraujordono@gmail.com>
2026-04-05 13:04:42 +0000

    HE temrinado el TAD link es un comentario preambulo

--- links.c
+++ links.c
@@ -11,13 +11,13 @@
  #include "links.h"
 
  struct _Links{
+    char*       name; 
     Direction   direction; 
     Id          id; 
     Id          origin; 
     Id          destiny; 
-    char*       name; 
-    Status      status_orig;
-    Status      status_dest;
+    Bool      	status_orig;
+	Bool		status_dest;
 };
 
 
@@ -134,7 +134,7 @@ Status       link_set_direction(Links *link, Direction dir)
 }
 Direction    link_get_direction(Links *link)
 {
-    if(!link) return FALSE;
+    if(!link) return U;
     return link->direction;
 }
 
@@ -155,19 +155,94 @@ Direction    link_get_oppsite_direction(Links *link)
 
 Status       link_set_origin_id(Links *link, Id id_orig)
 {
-    if(!link || id_orig == U) return ERROR;
-    
-   link->origin = id_orig;
+    if(!link || id_orig == NO_ID) return ERROR;
+    link->origin = id_orig;
+    return OK;
 }
-Id           link_get_origin_id(Links *link);
+Id           link_get_origin_id(Links *link)
+{
+    if(!link) return NO_ID;
+    return link->origin;
+}
 
-Status       link_set_destination_id(Links *link, Id id_dest);
-Id           link_get_destination_id(Links *link);
+Status       link_set_destination_id(Links *link, Id id_dest)
+{
+    if(!link || id_dest == NO_ID) return ERROR;
+    link->destiny = id_dest;
+    return OK;
+}
+Id           link_get_destination_id(Links *link)
+{
+    if(!link) return NO_ID;
+    return link->destiny;
+}
 
 
 
 /* ============== PRINT ====================== */
 
-Status       link_print(Links *link);
+Status       link_print(Links *link, FILE* output)
+{
+    if(!output){
+		printf( ROJO "ERROR: " RESET "There is nothing on the output \n");
+		return	ERROR;
+	}else if(!link){
+		fprintf( output,ROJO "ERROR: " RESET "There is nothing on the pointer to Link\n");
+		return	ERROR;
+	}
+
+	
+	Id			id_link = link_get_id(link);
+    Id          id_orig = link_get_origin_id(link);
+    Id          id_dest = link_get_destination_id(link);
+	Bool		open_orig = link_get_open_dest_to_origin(link);
+	Bool		open_dest = link_get_open_origin_to_dest(link);
+    Direction   dir = link_get_direction(link); 
+    Direction   dir_opposite = link_get_oppsite_direction(link);
+	char*		name= link_get_name(link);
+
+	/*DEBUG*/
+	short result = 0;
+
+	if(dir == U || dir_opposite == U){
+		free(name);
+		fprintf(output, "There is a problem whit dir or dir_opp\n");
+		result = 1;
+	}
+	if(id_link == NO_ID  ){
+		free(name);
+		fprintf(output, "There is a problem whit Id of the link\n");
+		result = 1;
+	}
+	if(id_dest == NO_ID || id_orig == NO_ID){
+		free(name);
+		fprintf(output, "There is a problem whit ids of orig or dest\n");
+		result = 1;
+	}
+	if(!name ){
+		free(name);
+		fprintf(output, "There is a problem whit name\n");
+		result = 1;
+	}
+	if(result) return ERROR;
+ 	/*PRINT ALL GOOD*/
+
+
+    fprintf(output,"=============== "ROJO" LINK "RESET"==================\n");
+	fprintf(output,"==\t\t>>NAME LINK    : %10s\t\t=\n", name);
+	fprintf(output,"==\t\t>>ID   LINK    : %4ld\t\t=\n", id_link);
+	fprintf(output,"==\t\t>>ID ORIGIN    : %4ld\t\t=\n", id_orig);
+    fprintf(output,"==\t\t>>ID DESTINI   : %4ld\t\t=\n", id_dest);
+    fprintf(output,"==\t\t>>DIRECTION    : %4d\t\t=\n", dir);
+    fprintf(output,"==\t\t>>OPEN DEST?   : %4d\t\t=\n", open_dest);
+    fprintf(output,"==\t\t>>DIRECTION OP : %4d\t\t=\n", dir_opposite);
+    fprintf(output,"==\t\t>>OPEN ORIG?   : %4d\t\t=\n", open_orig);
+    fprintf(output,"===============================================\n");
+
+
+
+	free(name);
+	return OK;
+}
 
  

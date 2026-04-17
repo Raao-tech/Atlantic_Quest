78d974242b5cec75b22d79ce2825d9f173bb642e
Raao-tech <rafaelaraujordono@gmail.com>
2026-04-18 01:54:35 +0200

    pequeño retoque a bash, aun le falta, dame unas dos horitas más

--- automatic_test.sh
+++ automatic_test.sh
@@ -3,13 +3,12 @@ choice=3
 name_file="*_test.c"
 
 echo -e "Hello! Please, tell me what do yo need to make?\n"
-while [ $choice -q 3 ]; do
-    echo -e "0 . Without Valgrind\n"
+while [[ ! $choice == 0  &&  ! $choice == 1 ]]; do
+    echo -e "0 . Without Valgrind"
     echo -e "1 . With    Valgrind\n"
     echo -e "Format of the input:   <your choice> <name_test_file_without_extension>\n"
     read -p "prompt >  " choice name_file _; #lo primero se guarda en choice, lo segundo e name_file como str, y lo que le sigue se ignora
-
-    if [ [$choice -q 0] || [$choice -q 1] ]; then
-        
-    fi
 done
+cd ./tests/
+make run_${name_file}
+echo -e "su eleccion fue ${choice} y su archivo es : ${name_file}\n";

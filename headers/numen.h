a55fc16a85822d2fc309e6dc2dd041092ad66888
Raao-tech <rafaelaraujordono@gmail.com>
2026-04-18 16:51:35 +0200

    creado, todavia no iniciado, el mpdulo numen

---
+++ headers/numen.h
@@ -0,0 +1,18 @@
+/**
+ * @brief It defines the character module interface
+ *
+ * @file character.h
+ * @author Violeta, Rafa y Salvador
+ * @version 3
+ * @date 16-04-2025
+ * @copyright GNU Public License
+ */
+
+#ifndef NUMEN_H
+#define NUMEN_H
+
+#include "headers/character.h"
+#include "headers/types.h"
+
+typedef struct _Character Character;
+#endif

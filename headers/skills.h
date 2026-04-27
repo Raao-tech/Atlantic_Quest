f1b66d1185202bee336d32ac8e26168514227205
Ao6400 <salvador.alcala.alvarez@outlook.com>
2026-04-27 11:57:18 +0200

    Define new skills and update skills struct
    Added new skills and updated the skills struct.

--- headers/skills.h
+++ headers/skills.h
@@ -1,3 +1,19 @@
+**
+ * @brief It defines the skills struct
+ *
+ * @file skills.h
+ * @author Javier Jarque and Rafael
+ * @version 1.2
+ * @date 20-04-2026
+ * @copyright GNU Public License
+ */
+
+
+
+#ifndef SKILLS_H
+#define SKILLS_H
+
+#include "types.h"
 #include "numen.h"
 
 
@@ -6,7 +22,9 @@ typedef enum{
     NO_SKILL,
     TAKLE,
     QUICK_ATTACK,
-    STEAM_ERUPTION
+    STEAM_ERUPTION,
+    STAND_PLATINUM,
+    THUNDER_SHOCK
 }Skills_id;
 
 struct _Skills
@@ -14,7 +32,6 @@ struct _Skills
     Skills_id     id;
     char* name;
     int     radio;
-    int     distancia;
     int    damage;
     int     coste;
 
@@ -29,4 +46,23 @@ typedef struct _Skills Skills;
 *    attack name_numen 2 1 
 */
 
+/**
+ * @brief It active the skill
+ * @author Javier Jarque and Rafael
+ *
+ * @param sender a pointer with the sender of the skill
+ * @param reciver a pointer with the reciver of the skill
+ * @param id_skill the id of the skill
+ * @param distance the distance of the skill
+ */
+void skill_active(Numen * sender, Numen* receiver, Skills_id id_skill, int distance);
+/**
+ * @brief It gets the name of the skill
+ * @author Javier Jarque
+ *
+ * @param id the id of the skill
+ * @return the name of the skill
+ */
+char *skill_get_name(Skills_id id);
 
+#endif
